08cd1755722a10b73b812c4a6cc55193e6c27adc
dat.tran <dat.tran@setacinq.vn>
2019-04-04 14:28:38 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/component/header.component';
+import { SidebarComponent } from './sidebar/component/sidebar.component';
+import { HomeComponent } from './home/component/home.component';
+import { ProductsComponent } from './products/component/products.component';
+import { NewsletterComponent } from './newsletter/component/newsletter.component';
+import { FooterComponent } from './footer/component/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    HomeComponent,
+    ProductsComponent,
+    NewsletterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule

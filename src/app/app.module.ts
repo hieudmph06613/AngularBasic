91d5c9588d06f02234c451d0f9416ae6e2dd0ed5
daibvph05803 <buidai856@gmail.com>
2019-04-12 19:36:58 +0700

    bài

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ProductComponent } from './component/product/product.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ProductDetailComponent } from './component/product-detail/product-detail.component';
+import { AddComponent } from './component/add/add.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './component/about/about.component'
+import { NotFoundComponent } from './component/not-found/not-found.component';
+import { HomeComponent } from './component/home/home.component'
+import {HttpClientModule} from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    FooterComponent,
+    ProductDetailComponent,
+    AddComponent,
+    AboutComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

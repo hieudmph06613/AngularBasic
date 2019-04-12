9db8cc5542c892729d94db0acd0ad43a36dfaa2a
maituyen2006 <maivantuyen2006@gmail.com>
2019-04-12 19:15:57 +0700

    em chào anh

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to AngularBasic!');
+    expect(page.getTitleText()).toEqual('Welcome to Web207Lab4!');
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailProductComponent } from './product/detail-product/detail-product.component';
+import { AddProductComponent } from './product/add-product/add-product.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './NotFound/NotFound.Component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    FooterComponent,
+    DetailProductComponent,
+    AddProductComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
